7736776e9e09a0d90fb6ae566e63de9eb756cd4b
famanakis <73799603+famanakis@users.noreply.github.com>
2023-04-17 21:14:05 -0600

    commit

---
+++ random-quote-machine/index.js
@@ -0,0 +1,11 @@
+
+
+const api = fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+  console.log(api)
